3697e6dc201c2013585a2cbf3dd77bcdd68bae12
MikhailPonomarev <michailp800@gmail.com>
2021-11-23 23:09:21 +0300

    Added admin-sidebar test

--- src/test/java/ru/stqa/litecart/AdminLoginDifferentBrowsers.java
+++ src/test/java/ru/stqa/litecart/AdminLoginDifferentBrowsers.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 
 
 public class AdminLoginDifferentBrowsers {
---
+++ src/test/java/ru/stqa/litecart/AdminSidebar.java
@@ -0,0 +1,56 @@
+package ru.stqa.litecart;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+
+public class AdminSidebar {
+    private WebDriver driver;
+    public WebDriverWait wait;
+
+    @BeforeAll
+    public static void setUp() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setUpChromeDriver() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+
+    @Test
+    @DisplayName("Should click all Side Buttons")
+    public void sideButtonsTest() {
+        driver.get("http://localhost/litecart/admin");
+        driver.findElement(By.cssSelector("[data-type='text']")).sendKeys("admin");
+        driver.findElement(By.cssSelector("[data-type='password']")).sendKeys("admin");
+        driver.findElement(By.cssSelector("[type='submit']")).click();
+
+        List<WebElement> sideButtons = driver.findElements(By.cssSelector("[id='app-']"));
+
+        for (int i = 0; i < sideButtons.size();i++) {
+            sideButtons = driver.findElements(By.id("app-"));
+            sideButtons.get(i).click();
+            String actual = driver.findElement(By.tagName("h1")).getTagName();
+            String expected = "h1";
+            Assertions.assertEquals(expected, actual);
+        }
+
+        driver.findElement(By.cssSelector("[title='Logout']"));
+    }
+}
